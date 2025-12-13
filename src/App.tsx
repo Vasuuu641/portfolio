8f95ed6e7402143ce6fd454999c39299989786a2
Vasundhara Ravi <vasundhararavi641@gmail.com>
2025-12-13 19:16:45 +0100

    Added homepage component to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,22 @@ import { Routes } from 'react-router-dom';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Connect from './pages/Connect';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="Portfolio Main Page">
       <main>
         <BrowserRouter>
+        
         <Navbar />
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/connect" element={<Connect />} />
         </Routes>
+
+        < Home  />
         </BrowserRouter>
 
       </main>
